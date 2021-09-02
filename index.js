ec14f59b4a5380dd7e58a367d3f76e8dc4b2ca03
proquicklearner <miyatrayash@gmail.com>
2021-09-02 23:56:15 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,40 @@
+/** @format */
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Connect to the database
+mongoose
+	.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("Database connected successfully"))
+	.catch((err) => console.error(err));
+
+// Since mongoose's Promise is deprecated, we override it with Node's Promise
+mongoose.Promise = global.Promise;
+
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	next();
+});
+
+app.use(bodyParser.json());
+
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	next();
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
